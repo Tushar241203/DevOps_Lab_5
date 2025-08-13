fa46c2d161a7c983d1c7bb2ee13b5af0a11e010e
Srishti Parulekar <srishtiparulekar430@gmail.com>
2025-08-13 16:02:55 +0530

    project 4

---
+++ docker-assignments/project-4-jnk-mvn-pipeline/maven-project/src/test/java/demo/AppTest.java
@@ -0,0 +1,11 @@
+package demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void testSum() {
+        assertEquals(4, 2 + 2);
+    }
+}
