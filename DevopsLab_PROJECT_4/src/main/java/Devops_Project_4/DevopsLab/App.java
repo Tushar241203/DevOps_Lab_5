8c1145950bf43b2d7068aba0478131da4db86e41
soumili-03 <soumili.biswas3183@gmail.com>
2025-09-01 20:14:10 +0530

    project 4 first commit

---
+++ DevopsLab_PROJECT_4/src/main/java/Devops_Project_4/DevopsLab/App.java
@@ -0,0 +1,13 @@
+package Devops_Project_4.DevopsLab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
